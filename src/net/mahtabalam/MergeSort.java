9faf85c5f67f28c23dc4886eb10d734177d04e1f
Mahtab Alam <alammahtab08@gmail.com>
2019-03-23 22:14:33 +0530

    Added Merge Sort implementation

---
+++ src/net/mahtabalam/MergeSort.java
@@ -0,0 +1,64 @@
+package net.mahtabalam;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		// int[] a = { 5, 1, 6, 2, 3, 4 };
+		 int []a = { 15, 1, 63, 20, 31, -4 };
+		// int []a = { 5, 1, 63, -1, 31, -4, 0 };
+		// int []a = { 9, 1, 8, -1, 7, -4, 6 };
+		// int[] a = { 36, 30, 1 };
+		mergeSort(a);
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i] + " ");
+	}
+
+	public static void mergeSort(int[] array) {
+		System.out.println("sort(" + Arrays.toString(array) + ")");
+		int n = array.length;
+		if (n == 1)
+			return;
+		int mid = n / 2;
+		int[] leftSubArray = new int[mid];
+		int[] rightSubArray = new int[n - mid];
+
+		for (int i = 0; i < mid; i++) {
+			leftSubArray[i] = array[i];
+		}
+		for (int i = mid; i < n; i++) {
+			rightSubArray[i - mid] = array[i];
+		}
+		mergeSort(leftSubArray);
+		mergeSort(rightSubArray);
+
+		merge(array, leftSubArray, rightSubArray);
+	}
+
+	public static void merge(int[] array, int[] leftArr, int[] rightArr) {
+
+		System.out.println("merge(" + Arrays.toString(array) + ", " +Arrays.toString(leftArr) + ", "
+				+ Arrays.toString(rightArr) + " )");
+
+		int left = leftArr.length;
+		int right = rightArr.length;
+
+		int i = 0, j = 0, k = 0;
+
+		while (i < left && j < right) {
+
+			if (leftArr[i] <= rightArr[j])
+				array[k++] = leftArr[i++];
+			else
+				array[k++] = rightArr[j++];
+
+		}
+
+		while (i < left)
+			array[k++] = leftArr[i++];
+
+		while (j < right)
+			array[k++] = rightArr[j++];
+	}
+}
